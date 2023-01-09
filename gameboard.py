e9dcd82085904f05f19af51d8ea15e418fb9b5be
davidvhulst <117074945+davidvhulst@users.noreply.github.com>
2023-01-09 16:32:06 +0100

    Update gameboard.py
    Werkt nu met de drie eerste gameboards

--- gameboard.py
+++ gameboard.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('data/Rushhour6x6_1.csv')
+# df = pd.read_csv('data/Rushhour6x6_2.csv')
+# df = pd.read_csv('data/Rushhour6x6_3.csv')
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,11 +19,30 @@ def gameboard(N, dataframe):
     Returns the Rush Hour gameboard
     """
     
+    # list with random colors
+    color_list = [[0.5, 0.5, 0], [0, 1, 0], [0, 0.5, 0], [0, 1, 1],\
+    [0, 0.5, 0.5], [0, 0, 1], [0, 0, 0.5], [1, 0, 1], \
+    [0.5, 0, 0.5], [0.5,0.5,0.5], [1,1,0], [1, 0.5, 0]]
+
+    # create an NxN gameboard with an edge 1 and an RGB color channel
     gameboard = np.zeros((N+2, N+2, 3))
     gameboard[1:N+1, 1:N+1] = gameboard[3,N+1] = 1
 
+    for i, row in dataframe.iterrows():
+        x = row['col']
+        y = row['row']
 
-    gameboard[3,1:3] = [1,0,0]
+        length = row['length']
+
+        if row['car'] != 'X':
+            if row['orientation'] == 'H':
+                gameboard[y,x:x+length] = color_list[i]
+
+            else:
+                gameboard[y:y+length,x] = color_list[i]
+
+        else:
+            gameboard[y,x:x+length] = [1,0,0]
 
     return gameboard
 
