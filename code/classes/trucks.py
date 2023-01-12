718d82d3224413c31c770526f8be203e7339978f
Jaap Hulsbosch <jrhulsbosch@gmail.com>
2023-01-12 20:30:04 +0100

    d

---
+++ code/classes/trucks.py
@@ -0,0 +1,8 @@
+class Trucks:
+    def __init__(self, name, x , y, orientation, length):
+
+        self.name = name
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+        self.length = length 
