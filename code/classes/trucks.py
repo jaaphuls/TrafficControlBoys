265dd67fe46c7408d469718198761465c64dc18f
Florianketelaar <117091622+Florianketelaar@users.noreply.github.com>
2023-01-13 13:41:05 +0100

    gastuh!!

--- code/classes/trucks.py
+++
@@ -1,8 +0,0 @@
-class Trucks:
-    def __init__(self, name, x , y, orientation, length):
-
-        self.name = name
-        self.x = x
-        self.y = y
-        self.orientation = orientation
-        self.length = length 
---
+++ code/classes/vehicle.py
@@ -0,0 +1,27 @@
+class vehicle:
+
+    def __init__(self, name, column, row, orientation, length):
+        self.name = name
+        self.x = column
+        self.y = row
+        self.orientation = orientation
+        self.length = length 
+        self.location_vehicle()
+
+    def location_vehicle(self):
+
+        self.area = []
+
+        for i in range(self.length):
+
+            if self.orientation == "H":
+                temp_x = self.x
+                temp_y = self.y
+                self.area.append((temp_x, temp_y)) 
+                temp_x += 1
+            
+            if self.orientation == "V":
+                temp_x = self.x
+                temp_y = self.y
+                self.area.append((temp_x, temp_y)) 
+                temp_y += 1
