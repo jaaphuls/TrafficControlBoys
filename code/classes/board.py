b4171f9459b75c52848ad5ddecb9cab237f87268
BadMenFinance <97311848+BadMenFinance@users.noreply.github.com>
2023-01-13 15:58:07 +0100

    co authored by davide

---
+++ code/classes/board.py
@@ -0,0 +1,10 @@
+class create_board:
+
+    def __init__(self, N):
+        rush_board = np.zeros(N + 2, N + 2)
+
+        return rush_board
+
+    def create_state(self):
+
+        for i in vehicle
