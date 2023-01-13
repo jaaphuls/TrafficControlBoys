b57a86e84c049273d438df3a6393a2d48017f24f
Florianketelaar <117091622+Florianketelaar@users.noreply.github.com>
2023-01-13 11:45:24 +0100

    ben de code van david aan het aanpassen

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -16,12 +16,16 @@ def gameboard(input_filename):
     dataframe = pd.read_csv(input_filename)
     
     # find the width and height N of the gameboard
+
+    '''
     # the slicing is corrected for N > 9
     if len(input_filename) == 22:
         N = int(input_filename[15])
 
     else:
         N = int(input_filename[16:18])
+
+    '''
 
     # create an NxN gameboard with a width 1 edge and an RGB color channel
     gameboard = np.zeros((N+2, N+2, 3))
