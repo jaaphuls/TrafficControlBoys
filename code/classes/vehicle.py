05a8af1f32d23f8da7e6f9380fa003d84126b1e3
davidvhulst <davidvanhulst@live.nl>
2023-01-16 12:04:32 +0100

    Color attribute

--- code/classes/vehicle.py
+++ code/classes/vehicle.py
@@ -1,14 +1,15 @@
+import random
 
 class Vehicle:
 
     def __init__(self, car, orientation, col, row, length):
-        self.color = [0,0,0]
         self.car = car
         self.x = col
         self.y = row
         self.orientation = orientation
         self.length = length 
         self.location_vehicle()
+        self.color_vehicle()
 
     def location_vehicle(self):
 
@@ -20,7 +21,29 @@ class Vehicle:
                 self.area.append((self.x, self.y))
                 self.x += 1
                 
+            
             else:
                 self.area.append((self.x, self.y))
                 self.y += 1
+    
+    def color_vehicle(self):
+
+        if self.car == 'X':
+            r, g, b = 1, 0, 0
+
+        else:
+
+            # create random color values
+            # the red value is limited to prevent red cars from appearing
+            r = random.uniform(0,0.7)
             
+            # cars with different lengths are given a slightly different color
+            if self.length == 2:
+                g = random.uniform(0,0.8)
+                b = random.random()
+
+            else:
+                g = random.random()
+                b = random.uniform(0,0.8)
+    
+        self.color = [r, g, b]
