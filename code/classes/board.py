bdaa10641414657a316bba5281306cf233746e89
Jaap Hulsbosch <jrhulsbosch@gmail.com>
2023-01-16 13:45:36 +0100

    check move

--- code/classes/board.py
+++ code/classes/board.py
@@ -20,12 +20,32 @@ class create_board:
             
                 # self.rush_board = self.rush_board[car.area]
 
+
     def check_move(self):
 
         for car in self.car_list:
-            
- 
-    
 
-        
+            # check in what orientation the car should move
+            if car.orientation == 'H': 
+
+                if gameboard[car.x + 1, car.y] == [1, 1, 1]:  
+                    return True 
+
+                elif gameboard[car.x - 1, car.y] == [1, 1, 1]:  
+                    return True
+            
+            # if the orientation is not horizontal, move on the y-axis
+            else: 
+
+                # give the same chance of 0.5 to move towards the top or the bottom
+                if gameboard[car.x, car.y + 1] == [1, 1, 1]:
+                    return True
+     
+                elif gameboard[car.x, car.y - 1] == [1, 1, 1]:
+                    return True
+
+        return False
+            
+
+                
       
