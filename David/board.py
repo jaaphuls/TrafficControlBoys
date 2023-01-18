ad339963cd0fa8f6e901f8bed7451e4454fe3737
davidvhulst <davidvanhulst@live.nl>
2023-01-18 23:08:36 +0100

    borders weggehaald

--- David/board.py
+++ David/board.py
@@ -1,44 +1,32 @@
 import numpy as np
 import pandas as pd
 import random
+import copy
 
 from vehicle import Vehicle
 
 class Board:
 
     def __init__(self, N):
-        self.occupied = self.borders(N)
-        self.rush_board = np.zeros((N + 2, N + 2), dtype=Vehicle)
-    
-    def borders(self, N):
+        self.rush_board = np.zeros((N, N), dtype=Vehicle)
 
-        borders = set()
-
-        for x in range(N + 2):
-            borders.add((0, x))
-            borders.add((N + 1, x))
-
-        for y in range(N + 2):
-            borders.add((y, 0))
-            borders.add((y, N + 1))
-        
-        return borders
-
-    def initialize_car(self, vehicle, x, y):
+    def initialize(self, vehicle, x, y):
 
         for i in range(vehicle.length):
 
             if vehicle.orientation == "H":
-                self.rush_board[y, x] = vehicle.car
+                self.rush_board[y, x] = vehicle
                 self.occupied.add((y, x))
                 x += 1
             
             else:
-                self.rush_board[y, x] = vehicle.car
+                self.rush_board[y, x] = vehicle
                 self.occupied.add((y, x))
                 y += 1
 
-    def update_car(self, x, y, car, backwards):
+    def update_car(self, x, y, backwards=False):
+
+        car = self.rush_board[y, x]
 
         car_front = car.length - 1
 
@@ -60,5 +48,17 @@ class Board:
             else:
                 self.rush_board[y, x] = 0
                 self.rush_board[y + car.length, x] = car
+
+    def visualize(self):
+        visualized = copy.copy(self.rush_board)
+        for y, row in enumerate(visualized):
+            for x, coord in enumerate(row):
+                if type(coord) == Vehicle:
+                   visualized[y,x] = coord.car
+
+                else:
+                    visualized[y,x] = '.'
+
+        print(visualized)
 
 
