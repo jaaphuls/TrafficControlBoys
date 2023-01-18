c510d76a7d7595f2646c9a39b4b14ef108f73992
davidvhulst <davidvanhulst@live.nl>
2023-01-18 23:10:39 +0100

    handmatige zetten geprobeerd

--- David/main.py
+++ David/main.py
@@ -3,19 +3,27 @@ import numpy as np
 from board import Board
 from vehicle import Vehicle
 
-csv = 'data/Rushhour6x6_4.csv'
+csv = 'data/Rushhour6x6_1.csv'
 
 # read the csv input file as a pandas dataframe
 dataframe = pd.read_csv(csv)
 
-board = Board(9)
+board = Board(6)
 
 for i, row in dataframe.iterrows():
     car = Vehicle(row['car'], row['orientation'], row['length'])
 
-    x = row['col']
-    y = row['row']
+    x = row['col'] - 1
+    y = row['row'] - 1
 
-    board.initialize_car(car, x, y)
+    board.initialize(car, x, y)
 
-print(board.rush_board)
+updates = [(1,0), (1,1), (2,2), (2,1), (4,4), (5,2)]
+booleans = [True, True, True, True, True, False]
+
+for update, boolean in zip(updates, booleans):
+    x, y = update
+    board.update_car(x, y, backwards=boolean)
+    board.visualize()
+
+
