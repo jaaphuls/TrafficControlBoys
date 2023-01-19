82525a2429effc53a2cff01c8155c9609dc3292e
Florianketelaar <117091622+Florianketelaar@users.noreply.github.com>
2023-01-19 14:12:10 +0100

    d

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -22,50 +22,50 @@ class Random():
         if car.orientation == 'H': 
             x_left = (car.x - 1)
             x_right = (car.x + car.length)
-            print(f'this is left {x_left}')
-            print(f'this is right {x_right}')
+            # print(f'this is left {x_left}')
+            # print(f'this is right {x_right}')
             
             if x_left >= 1: 
                 if self.rush_board[car.y, x_left] == '.' and fifty_fifty < 0.50:
                     car.x -= 1
                 else:
-                    print("pass 1 left")
+                    # print("pass 1 left")
                     pass
 
             if x_right <= self.N - 1: 
                 if self.rush_board[car.y, x_right] == '.' and fifty_fifty > 0.50:
                     car.x += 1
                 else:
-                    print("pass 1 right")
+                    # print("pass 1 right")
                     pass
 
             else:
-                print("pass2")
+                # print("pass2")
                 pass
 
         # car oriention 'V'
         else:
             y_down = (car.y + car.length)
             y_up = (car.y - 1) 
-            print(f'this is up {y_up}')
-            print(f'this is down {y_down}')
+            # print(f'this is up {y_up}')
+            # print(f'this is down {y_down}')
 
             if y_down <= self.N - 2:
                 if self.rush_board[y_down, car.x] == '.' and fifty_fifty > 0.5:  
                     car.y += 1
                 else:
-                    print("pass")
+                    # print("pass")
                     pass
 
             elif y_up >= 1:
                 if self.rush_board[y_up, car.x] == '.' and fifty_fifty < 0.5: 
                     car.y -= 1
                 else:
-                    print("pass")
+                    # print("pass")
                     pass
 
             else:
-                print("pass")
+                # print("pass")
                 pass
 
         return self.car_list
