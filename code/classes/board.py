748f79401884827c3eaa755d3016dff5c6891d5c
Jaap Hulsbosch <jrhulsbosch@gmail.com>
2023-01-20 11:29:10 +0100

    commit

--- code/classes/board.py
+++ code/classes/board.py
@@ -105,37 +105,12 @@ class Board:
                     pass
                 
             return possible_boards
-
-    # def check_move(self, car):
-
-    #     x_left = (car.x - 1)
-    #     x_right = (car.x + car.length)
-    #     y_down = (car.y + car.length)
-    #     y_up = (car.y - 1) 
-        
-    #     if car.orientation == "H":
-        
-    #         if self.rush_board[car.y, car.x - 1] == "." or self.rush_board[car.y, car.x - 1] == car.car and x_left >= 1:
-    #             return -1
-    #         if self.rush_board[car.y, car.x + 1] == "." or self.rush_board[car.y, car.x + 1] == car.car and x_right <= self.N - 1:
-    #             return 1
-    #         else:
-    #             return 0
-
-    #     else:
-    #         if self.rush_board[car.y -1, car.x] == "." or self.rush_board[car.y -1, car.x] == car.car and y_down <= self.N - 1:
-    #             return 1
-    #         if self.rush_board[car.y + 1, car.x] == "." or self.rush_board[car.y + 1, car.x] == car.car and y_up >= 1:
-    #             return -1
-    #         else:
-    #             return 0
     
     def visualize(self):
         possible_moves = []
 
         for row in range(len(self.rush_board)):
             for column in range(len(self.rush_board)):
-
 
                 if self.rush_board[row, column] == 0:
                    possible_moves.append((row, column))
