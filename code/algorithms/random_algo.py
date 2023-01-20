99b8e6e29f4ee84fa5a8f896239ca145a8635985
BadMenFinance <97311848+BadMenFinance@users.noreply.github.com>
2023-01-20 10:42:10 +0100

    sam heeft heel veel werk gedaan

--- code/algorithms/random_algo.py
+++ code/algorithms/random_algo.py
@@ -1,4 +1,5 @@
 import random
+import time
 from code.classes.vehicle import Vehicle
 from code.classes.board import Board
 
@@ -8,82 +9,100 @@ from code.classes.board import Board
 #         self.board = board
 #         self.random_step()
         
+def random_step(board, N):
+    start_board = board
+    print(start_board)
+    start_time = time.time()
+    steps = 0
+    while True:
+        start_board = Board(random.choice(start_board.check_move()), N)
+        steps += 1
+        start_board.create_state()
+        start_board.create_board()
+        start_board.visualize()
 
-def random_step(board):
+        if board.rush_board[2, 5] == "X":
+            
+            print(f'the total steps were {step}')
+            print(f"solvetime = {time.time() - start_time} seconds")
+            
+            return exit()
 
-    car = random.choice(board.car_list)
-    fifty_fifty = random.random()
-    #print(fifty_fifty)
-    #print(f' this is car.x: {car.x}')
-    #print(f' this is car.y: {car.y}')
+# def random_step(board):
+#     possible_board = []
+#     car = random.choice(board.car_list)
+#     fifty_fifty = random.random()
+#     #print(fifty_fifty)
+#     #print(f' this is car.x: {car.x}')
+#     #print(f' this is car.y: {car.y}')
 
-    # mover = board.check_move(car)
+#     # mover = board.check_move(car)
 
-    # if mover == 1 and car.orientation == "H":
-    #     car.x += 1
+#     # if mover == 1 and car.orientation == "H":
+#     #     car.x += 1
 
-    # elif mover == -1 and car.orientation == "H":
-    #     car.x -= 1
+#     # elif mover == -1 and car.orientation == "H":
+#     #     car.x -= 1
 
-    # elif mover == 1 and car.orientation == "V":
-    #     car.y -= 1
+#     # elif mover == 1 and car.orientation == "V":
+#     #     car.y -= 1
 
-    # elif mover == -1 and car.orientation == "V":
-    #     car.y += 1
+#     # elif mover == -1 and car.orientation == "V":
+#     #     car.y += 1
 
-    if car.orientation == 'H': 
-        x_left = (car.x - 1)
-        x_right = (car.x + car.length)
-        print(f'this is left {x_left}')
-        print(f'this is right {x_right}')
+#     if car.orientation == 'H': 
+#         x_left = (car.x - 1)
+#         x_right = (car.x + car.length)
+#         print(f'this is left {x_left}')
+#         print(f'this is right {x_right}')
         
-        if x_left >= 1: 
-            if board.rush_board[car.y, x_left] == '.' and fifty_fifty < 0.50:
-                car.x -= 1
-        # if x_left - 1 >= 1:
-        #     if board.rush_board[car.y, x_left-1] == '.' and fifty_fifty < 0.50:
-        #         car.x -= 2
-            else:
-                pass
+#         if x_left >= 1: 
+#             if board.rush_board[car.y, x_left] == '.' and fifty_fifty < 0.50:
+#                 car.x -= 1
+#         # if x_left - 1 >= 1:
+#         #     if board.rush_board[car.y, x_left-1] == '.' and fifty_fifty < 0.50:
+#         #         car.x -= 2
+#             else:
+#                 pass
 
-        if x_right <= board.N - 1: 
-            if board.rush_board[car.y, x_right] == '.' and fifty_fifty > 0.50:
-                car.x += 1
-        # if x_right + 2 <= board.N - 1: 
-        #     if board.rush_board[car.y, x_right + 2] == '.' and fifty_fifty > 0.75:
-        #         car.x += 2
+#         if x_right <= board.N - 1: 
+#             if board.rush_board[car.y, x_right] == '.' and fifty_fifty > 0.50:
+#                 car.x += 1
+#         # if x_right + 2 <= board.N - 1: 
+#         #     if board.rush_board[car.y, x_right + 2] == '.' and fifty_fifty > 0.75:
+#         #         car.x += 2
 
-            else:
-                print("pass 1 right")
-                pass
+#             else:
+#                 print("pass 1 right")
+#                 pass
 
-        else:
-            print("pass2")
-            pass
+#         else:
+#             print("pass2")
+#             pass
 
-    # car oriention 'V'
-    else:
-        y_down = (car.y + car.length)
-        y_up = (car.y - 1) 
-        print(f'this is up {y_up}')
-        print(f'this is down {y_down}')
+#     # car oriention 'V'
+#     else:
+#         y_down = (car.y + car.length)
+#         y_up = (car.y - 1) 
+#         print(f'this is up {y_up}')
+#         print(f'this is down {y_down}')
 
-        if y_down <= board.N - 1:
-            if board.rush_board[y_down, car.x] == '.' and fifty_fifty > 0.5:  
-                car.y += 1
-            else:
-                print("pass")
-                pass
+#         if y_down <= board.N - 1:
+#             if board.rush_board[y_down, car.x] == '.' and fifty_fifty > 0.5:  
+#                 car.y += 1
+#             else:
+#                 print("pass")
+#                 pass
 
-        elif y_up >= 1:
-            if board.rush_board[y_up, car.x] == '.' and fifty_fifty < 0.5: 
-                car.y -= 1
-            else:
-                print("pass")
-                pass
+#         elif y_up >= 1:
+#             if board.rush_board[y_up, car.x] == '.' and fifty_fifty < 0.5: 
+#                 car.y -= 1
+#             else:
+#                 print("pass")
+#                 pass
 
-        else:
-            print("pass")
-            pass
+#         else:
+#             print("pass")
+#             pass
 
-    return board.car_list
+#     return board.car_list
