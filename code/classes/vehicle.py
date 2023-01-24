fb9f74277296e4df350f3b6849fd6d6e72d7b5a5
Florianketelaar <117091622+Florianketelaar@users.noreply.github.com>
2023-01-24 15:08:41 +0100

    new auto

--- code/classes/vehicle.py
+++ code/classes/vehicle.py
@@ -8,27 +8,28 @@ class Vehicle:
         self.y = row -1
         self.orientation = orientation
         self.length = length 
+        #self.color = random.random()
         
-    #     self.color_vehicle()
+        self.color_vehicle()
     
-    # def color_vehicle(self):
+    def color_vehicle(self):
 
-    #     if self.car == 'X':
-    #         r, g, b = 1, 0, 0
+        if self.car == 'X':
+            r, g, b = 1, 0, 0
 
-    #     else:
+        else:
 
-    #         # create random color values
-    #         # the red value is limited to prevent red cars from appearing
-    #         r = random.uniform(0,0.7)
+            # create random color values
+            # the red value is limited to prevent red cars from appearing
+            r = random.uniform(0,0.7)
             
-    #         # cars with different lengths are given a slightly different color
-    #         if self.length == 2:
-    #             g = random.uniform(0,0.8)
-    #             b = random.random()
+            # cars with different lengths are given a slightly different color
+            if self.length == 2:
+                g = random.uniform(0,0.8)
+                b = random.random()
 
-    #         else:
-    #             g = random.random()
-    #             b = random.uniform(0,0.8)
+            else:
+                g = random.random()
+                b = random.uniform(0,0.8)
     
-    #     self.color = [r, g, b]
+        self.color = [r, g, b]
