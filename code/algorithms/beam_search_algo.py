37370394ab9f2342f89b1f18d771a570e44dbce2
Florianketelaar <117091622+Florianketelaar@users.noreply.github.com>
2023-01-26 13:41:58 +0100

    Merge remote-tracking branch 'refs/remotes/orig

---
+++ code/algorithms/beam_search_algo.py
@@ -0,0 +1,56 @@
+import random
+import time
+import queue
+from code.classes.vehicle import Vehicle
+from code.classes.board import Board
+
+# breadth first algorithm
+def beam_search(board, board_size):
+    start_time = time.time()
+    previous_states = board.states_list
+    previous_states.add(board.string_value)
+    # print(board.string_value)
+    # print(previous_states)
+    
+    step = 0
+
+    choices_queue = queue.Queue()
+    choices_queue.put(board)
+
+    
+    if board_size == 6: 
+        end_coord = 2,5
+
+    if board_size == 9: 
+        end_coord = 4,8
+    
+    if board_size == 12: 
+        end_coord = 5, 11
+
+
+    while True:
+        current_state = choices_queue.get()
+        
+        for states in current_state.check_move():
+            
+            new_game = Board(states, board_size)
+
+            if new_game.string_value not in board.states_list:
+                step += 1
+                print(step)
+                # print(new_game.string_value)
+                # print(board.states_list)
+                board.states_list.add(new_game.string_value)
+               
+                new_game.create_board()
+                new_game.visualize()
+
+                choices_queue.put(new_game)
+
+                if new_game.rush_board[end_coord] == "X":
+                    runtime = time.time() - start_time
+                    print(step)
+                    return runtime
+            else:
+                pass
+        
