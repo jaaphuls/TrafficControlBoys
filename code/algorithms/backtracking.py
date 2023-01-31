3833e77d77d1f0358f7409e75b471cd0d40030af
BadMenFinance <97311848+BadMenFinance@users.noreply.github.com>
2023-01-31 16:01:37 +0100

    whitespaces

--- code/algorithms/backtracking.py
+++ code/algorithms/backtracking.py
@@ -1,13 +1,9 @@
-
-
 def backtrace(dictionary, solution_state): 
     lol = [solution_state]
     
     while dictionary[solution_state] != None:
 
         solution_state = dictionary[solution_state]
-
         lol.append(solution_state)
-    
     
     return lol
